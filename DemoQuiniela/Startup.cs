617fc39122cc946a339fe56b890a2047c4dba216
Alfredo Morales <alfredo.57pro@gmail.com>
2018-03-07 19:49:35 -0600

    Inserción inicial en el repositorio del módulo DemoQuiniela

---
+++ DemoQuiniela/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoQuiniela.Startup))]
+namespace DemoQuiniela
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
