5366c0c111b33143f8f94815982b92e5db436193
Alfredo Morales <alfredo.57pro@gmail.com>
2018-03-10 01:26:47 -0600

    Template integrados para el ingreso de marcadores y la tabla de posiciones.

---
+++ DemoQuiniela/Controllers/QuinielaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoQuiniela.Controllers
+{
+    public class QuinielaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Posiciones()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult IngresoMarcadores()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
